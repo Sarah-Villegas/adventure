c8a18a5eca7fe92aaa8054444882714748d8575d
Nicole Hipolito <nicolebernadetteihipolito@student.hartnell.edu>
2015-10-09 11:03:34 -0700

    Added driver.cpp

---
+++ driver.cpp
@@ -0,0 +1,14 @@
+//Name: Nicole Hipolito
+//Name: Sarah Villegas
+//Name: Maria Chombo
+
+#include <iostream>
+#include <cassert>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+    return 0;   
+}
